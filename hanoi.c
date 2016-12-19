4f12af543d7a0cbff2c06596901f3459f965ba81
Vishwajeetdeulkar <vishwajeetd13@gmail.com>
2016-12-19 16:16:41 +0530

    Add files via upload

---
+++ hanoi.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+
+int toh(int n,char s,char t,char d)
+
+{
+
+if(n==1)
+{
+
+printf("\nMove disk %d from %c to %c \n",n,s,d);
+return(0);
+
+}
+
+toh(n-1,s,d,t);
+
+printf("\nMove disk %d from %c to %c \n",n,s,d);
+toh(n-1,t,s,d);
+
+return(0);
+
+}
+
+
+int main()
+
+{
+
+char s='A',d='C',t='B';
+
+int n;
+
+printf("\nEnter the number of disks:  ");
+
+scanf("%d",&n);
+
+printf("\nSequence of disks is:\n");
+
+toh(n,s,t,d);
+
+printf("\n");
+
+return(0);
+
+}
